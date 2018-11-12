9365204679ed1c5af9eef3cf42b161616ac02bfd
Ryan McCartney <rmccartney856@gmail.com>
2018-11-12 12:37:31 +0000

    Error Handling Implementaiton

--- navigation/main.py
+++ navigation/main.py
@@ -1,7 +1,8 @@
-#NAME:  main.py
-#AUTH:  Ryan McCartney, EEE Undergraduate, Queen's University Belfast
-#DESC:  A python class for aquiring image data from network streams
-#COPY:  Copyright 2018, All Rights Reserved, Ryan McCartney
+#NAME: main.py
+#DATE: 12/11/2018
+#AUTH: Ryan McCartney, EEE Undergraduate, Queen's University Belfast
+#DESC: Main python script for automous navigation of a two wheeled robot.
+#COPY: Copyright 2018, All Rights Reserved, Ryan McCartney
 
 from cameraData import cameraData
 from navigation import Navigation
@@ -12,95 +13,142 @@ import cv2 as cv
 import json
 import time
 
-#--------------------------------------------------------------------------------
-# Load user adjustable variables from json file
-#--------------------------------------------------------------------------------
+#Create a file for logging
+try:
+    #open a txt file to use for logging 
+    logFile = open("log.txt","w+")
+    currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+    logEntry = currentDateTime + ": " + "INFO = Program has started running." + "\n"
+    logFile.write(logEntry)
+    print(logEntry)
+except:
+    print("ERROR: Unable to open log file.")
+    exit()
 
-#open a txt file to use for logging 
-logFile = open("log.txt","w+")
-currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
-logEntry = currentDateTime + ": " + "INFO = Program has started running." + "\n"
-logFile.write(logEntry)
+#Import Settings File
+try: 
+    #load settings file
+    settingsFile = open('navigation\settings.json').read()
+    settings = json.loads(settingsFile)
+    #write status to log file
+    currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+    logEntry = currentDateTime + ": " + "INFO = Settings file loaded." + "\n"
+    logFile.write(logEntry)
+    print(logEntry)
+except:
+    #write status to log file
+    currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+    logEntry = currentDateTime + ": " + "ERROR = Unable to open the settings file." + "\n"
+    logFile.write(logEntry)
+    print(logEntry)
+    exit()
 
-#load settings file
-settingsFile = open('navigation\settings.json').read()
-settings = json.loads(settingsFile)
-
-#write status to log file
-currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
-logEntry = currentDateTime + ": " + "INFO = Settings file loaded." + "\n"
-logFile.write(logEntry)
-
-#Resolution of matrix
-unitSize = settings['map']['unitSize']
-
-#Map Length, Width and Height (in milimeters)
-mapLength = settings['map']['length']
-mapWidth = settings['map']['width']
-mapHieght = settings['map']['hieght']
-
-#write status to log file
-currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
-logEntry = currentDateTime + ": " + "INFO = Map created." + "\n"
-logFile.write(logEntry)
-
-#--------------------------------------------------------------------------------
-#Data Streams Setup
-#--------------------------------------------------------------------------------
-
-#Assign URLs from Settings File to Variables
-control_url = settings['host']['command_url']
-kinectImage_url = settings['host']['kinectImage_url']
-kinectDepth_url = settings['host']['kinectDepth_url']
-webcam_url = settings['host']['webcam_url']
-test_url = settings['host']['test_url']
-
-#write status to log file
-currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
-logEntry = currentDateTime + ": " + "INFO = Loading camera streams." + "\n"
-logFile.write(logEntry)
-
-#Class instances for various streams
-#kinectImage = cameraData(kinectImage_url,"Kinect RGB Data")
-kinectDepth = cameraData(test_url,"Kinect Depth Data")
-
-#Stream Data
-#kinectImage.streamVideo()
-#kinectDepth.streamVideo()
-
-#write status to log file
-currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
-logEntry = currentDateTime + ": " + "STATUS = Camera streams loaded." + "\n"
-logFile.write(logEntry)
+#Extract Data from settings file
+try: 
+    #Data from settings file
+    unitSize = settings['map']['unitSize']
+    mapLength = settings['map']['length']
+    mapWidth = settings['map']['width']
+    mapHieght = settings['map']['hieght']
+    control_url = settings['host']['command_url']
+    kinectImage_url = settings['host']['kinectImage_url']
+    kinectDepth_url = settings['host']['kinectDepth_url']
+    webcam_url = settings['host']['webcam_url']
+    test_url = settings['host']['test_url']
+    
+    #write status to log file
+    currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+    logEntry = currentDateTime + ": " + "INFO = Data extracted successfully from settings file." + "\n"
+    logFile.write(logEntry)
+    print(logEntry)
+except:
+    #write status to log file
+    currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+    logEntry = currentDateTime + ": " + "ERROR = Unable to extract data from settings file. Is data in the correct format?" + "\n"
+    logFile.write(logEntry)
+    print(logEntry)
+    exit()
 
 
+#Get camera data
+try: 
+    #Class instances for various streams
+    #kinectImage = cameraData(kinectImage_url,"Kinect RGB Data")
+    kinectDepth = cameraData(test_url,"Kinect Depth Data")
 
-#--------------------------------------------------------------------------------
+    #Stream Data
+    #kinectImage.streamVideo()
+    #kinectDepth.streamVideo()
+
+    #write status to log file
+    currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+    logEntry = currentDateTime + ": " + "INFO = Camera data acquired succesfully." + "\n"
+    logFile.write(logEntry)
+    print(logEntry)
+except:
+    #write status to log file
+    currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+    logEntry = currentDateTime + ": " + "ERROR = Unable to acquire camera data. Check stream URLs in settings file." + "\n"
+    logFile.write(logEntry)
+    print(logEntry)
+    exit()
+
 #Intialise Control Session for Wheelchair
-#--------------------------------------------------------------------------------
+try:
+    #Initialise Class for control
+    wheelchair = Control(control_url)
+    #write status to log file
+    currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+    logEntry = currentDateTime + ": " + "INFO = Control Connection Established with Robotic Device." + "\n"
+    logFile.write(logEntry)
+    print(logEntry)
 
-#Initialise Control of Wheelchair
-wheelchair = Control(control_url)
+    #Carry out control command
+    try:
+        #Ramp Speed
+        wheelchair.rampSpeed(100,100)
+    except:
+         #write status to log file
+        currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+        logEntry = currentDateTime + ": " + "ERROR = Unable to adjust wheelchair speed." + "\n"
+        logFile.write(logEntry)
+        print(logEntry)
 
-#Ramp Speed
-wheelchair.rampSpeed(100,100)
+except:
+    #write status to log file
+    currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+    logEntry = currentDateTime + ": " + "ERROR = Unable to establish connection with Robotic Deivce." + "\n"
+    logFile.write(logEntry)
+    print(logEntry)
 
-#--------------------------------------------------------------------------------
 #Intialise Navigation Techniques
-#--------------------------------------------------------------------------------
+try:
+    #Intialise Class
+    navigate = Navigation(unitSize,mapLength,mapWidth,mapHieght)
 
-#Initilise Class
-navigate = Navigation(unitSize,mapLength,mapWidth,mapHieght)
+    #Adjust Scall Factor to Improve Optimisation
+    navigate.scaleFactor = 2
+    navigate.fps = 60
+    delay = 1/navigate.fps
+    #Start Closest Point in Path Analysis
+    navigate.closestPoint(test_url,True)
+    
+    #write status to log file
+    currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+    logEntry = currentDateTime + ": " + "INFO = Navigation started sucessfully." + "\n"
+    logFile.write(logEntry)
+    print(logEntry)
 
-#Adjust Scall Factor to Improve Optimisation
-navigate.scaleFactor = 2
-navigate.fps = 60
-delay = 1/navigate.fps
-#Start Closest Point in Path Analysis
-navigate.closestPoint(test_url,True)
+except:
+    #write status to log file
+    currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+    logEntry = currentDateTime + ": " + "ERROR = Unable to begin navigation." + "\n"
+    logFile.write(logEntry)
+    print(logEntry)
+
+
 
 previousMaxSpeed = 0
-
 pointCloud = 0
 frames = 25
 
